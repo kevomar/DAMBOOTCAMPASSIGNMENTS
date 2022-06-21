6e5a2b89c0c1efb3afa6453ebd76974151f6cda0
Kelvin Maritim <>
2022-06-21 22:24:04 +0000

    second

--- Assets/main.js
+++
@@ -1,6 +0,0 @@
-function writeMessage(){
-    var name = document.getElementById('personName').value;
-    const nametag = document.createElement("p")
-    nametag.innerHTML = "Hello " + name + ", welcome to the bootcamp";
-    document.body.append(nametag)
-}
